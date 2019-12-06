d3cb87b0639dd107e8d41ea8a6650185235da324
yconsagous <yogesh.waghmare@consagous.com>
2019-12-06 18:41:08 +0530

    controller update

--- app/controllers/api/v1/category_controller.rb
+++ app/controllers/api/v1/category_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::CategoryController < ApplicationController
 	def index
-		@category_list = Category.all
-		puts "LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL#{@category_list.inspect}"
+		@category_list = Category.all.includes(:columns)
+		#@category_list = Category.eager_load(columns: {user: :group}).where('groups.id = ?', group.id)
+		render json: @category_list
+		@category_list.each do |cl|
+			puts "LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL#{cl.inspect}"
+		end
 	end
 end
